571d43cd73b4eedd9ca975008265c822ed2ddbe8
Will Koury <wkoury@hey.com>
2021-01-05 16:02:57 -0500

    Gets data from URL

---
+++ utils/url.js
@@ -0,0 +1,21 @@
+const request = require("request");
+
+const url = "https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/raw_data/vaccine_data_us_state_timeline.csv";
+
+
+exports.getData = function(){
+	return new Promise((resolve, reject) => {
+		request({uri: url}, function(error, response, body){
+			const lines = body.split("\n");
+		
+			let data = [];
+		
+			lines.forEach(line => {
+				let token = line.split(",");
+				data.push(token);
+			});
+
+			resolve(data);
+		});		
+	});
+}
