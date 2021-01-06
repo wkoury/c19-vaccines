6042660b1504292b8c9a2de6482bb912ff881777
Will Koury <wkoury@hey.com>
2021-01-05 20:49:23 -0500

    First working version

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,143 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+import { Alert, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Container, Row, Col } from "reactstrap";
+import {Line} from "react-chartjs-2";
 
-export default function Home({ data }) {
+
+export default function Home({ data, lastCommit }) {
+  //data init
+  let stateSet = new Set();
+  for(let i = 1; i < data.length; ++i){
+    stateSet.add(data[i][1]);
+  }
+  const states = Array.from(stateSet);
+
+  const [state, setState] = useState("Select a State");
+  const [dropdownOpen, setOpen] = useState(false);
+  const toggle = () => setOpen(!dropdownOpen);
+
+  let filteredData = data.filter(line => { return line[1] == state });
+  let filteredData2 = [];
+  filteredData.forEach(line => {
+    filteredData2.push([line[0], line[4], line[12]]);
+  });
+
+  const dataObject = {
+    labels: getColumnByIndex(filteredData2, 0),
+    datasets: [
+      {
+        label: "Vaccines Provided",
+        data: cleanZeros(getColumnByIndex(filteredData2, 1)),
+        fill: false,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgba(255, 99, 132, 0.2)",
+        yAxisID: "y-axis-1",
+      },
+      {
+        label: "Vaccines Administered",
+        data: cleanZeros(getColumnByIndex(filteredData2, 2)),
+        fill: false,
+        backgroundColor: "rgb(54, 162, 235)",
+        borderColor: "rgba(54, 162, 235, 0.2)",
+        yAxisID: "y-axis-2",
+      },
+    ]
+  }
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          type: "linear",
+          display: true,
+          position: "left",
+          id: "y-axis-1",
+          ticks: {
+            suggestedMax: Math.max.apply(Math, getColumnByIndex(filteredData2, 1)),
+            beginAtZero: true,
+          }
+        },
+        {
+          type: "linear",
+          display: true,
+          position: "left",
+          id: "y-axis-2",
+          ticks: {
+            suggestedMax: Math.max.apply(Math, getColumnByIndex(filteredData2, 1)),
+            beginAtZero: true,
+          }
+        }
+      ]
+    }
+  }
+  
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>COVID-19 Vaccine Tracker</title>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          {data.length}
-        </h1>     
+        <Alert color="info">
+          <h1>COVID-19 Vaccine Tracker</h1>
+        </Alert>
+        <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+          <DropdownToggle caret>
+            {state}
+          </DropdownToggle>
+          <DropdownMenu>
+            {states.map(state => (
+              <DropdownItem 
+                key={states.indexOf(state)}
+                onClick={() => setState(state)}
+              >
+                {state}
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </ButtonDropdown>
+        <div className={styles.chart}>
+          <Line data={dataObject} options={options}></Line>
+        </div>
+        <div className={styles.footer}>
+          <Container>
+            <Row>
+              <Col>Data last updated <a href="https://github.com/govex/COVID-19/blob/master/data_tables/vaccine_data/raw_data/vaccine_data_us_state_timeline.csv">{lastCommit}</a>.</Col>
+              <Col><a href="https://github.com/wkoury/c19-vaccines">GitHub</a></Col>
+            </Row>
+          </Container>
+        </div>
       </main>
     </div>
-  )
+  );
 }
 
-export async function getStaticProps() {
-  const dataFunc = require("../utils/url");
-  
+export async function getServerSideProps() {
+  const dataFunc = require("../utils/url");  
   const data = await dataFunc.getData();
-  
-  return { props: { data } }
+  const commitsRes = await dataFunc.getCommits();
+  const commits = JSON.parse(commitsRes);
+
+  return { props: { data, lastCommit: commits[0].commit.author.date } } //data is basically a giant 2D array of the csv file from https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/raw_data/vaccine_data_us_state_timeline.csv
+}
+
+const getColumnByIndex = (matrix, index) => { //matrix MUST be a 2D array
+  let ret = [];
+  matrix.forEach(row => {
+   ret.push(row[index]);
+  });
+
+  return ret;
+}
+
+//for vaccines provided, clean up zeroes
+const cleanZeros = array => {
+  for(let i = 0; i < array.length; ++i){
+    if(array[i] === "" && i > 0){
+      array[i] = array[i - 1];
+    }
+  }
+
+  return array;
 }
--- utils/url.js
+++ utils/url.js
@@ -2,7 +2,6 @@ const request = require("request");
 
 const url = "https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/raw_data/vaccine_data_us_state_timeline.csv";
 
-
 exports.getData = function(){
 	return new Promise((resolve, reject) => {
 		request({uri: url}, function(error, response, body){
@@ -16,6 +15,16 @@ exports.getData = function(){
 			});
 
 			resolve(data);
+		});		
+	});
+}
+
+const commitsUrl = "https://api.github.com/repos/govex/COVID-19/commits";
+
+exports.getCommits = function(){
+	return new Promise(resolve => {
+		request({uri: commitsUrl, headers: {"User-Agent": "request"}}, function(error, response, body){
+			resolve(body);
 		});		
 	});
 }
